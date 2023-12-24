16de4b7e84c76bb74a0e37a6a257a104ea50c09b
sadh19 <saulalberto19@gmail.com>
2023-12-23 23:09:05 -0600

    - ADD comments on merge sort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,11 +1,24 @@
-function merge(arrOne, arrTwo) {
+//Pseudocode
+//Combination of merging and sorting
+//Exploits the fact that arrays of 0/1 elements are sorted
 
+//1 - Split the array on half, and then again and again, until we have arrays with 1 elements.
+//2 - Merge and sort pairs of arrays until we only have arrays with two elements.
+//3- Repeat the process, to generate arrays of 4 elements, and so on and on.
+
+function merge(arrOne, arrTwo) {
+  //Create empty array that will be returned
   let result = [];
+
+  //Create pointers to initialize array position
   let indexOne = 0;
   let indexTwo = 0;
 
-  while (indexOne < arrOne.length && indexTwo < arrTwo.length) {
+  //If the current value of the first array is smaller than the current value of the second array, push the value in the fisrt array in the results array and move on to the next value in the first array.
+  //If the current value of the first array is greater than the current value of the second array, push the value in the second array in the results array and move on to the next value in the second array.
+  //Once we exhaust one array, push in all the remaining values from the other array.
 
+  while (indexOne < arrOne.length && indexTwo < arrTwo.length) {
     if (arrOne[indexOne] <= arrTwo[indexTwo]) {
       result.push(arrOne[indexOne]);
       indexOne++;
@@ -13,7 +26,6 @@ function merge(arrOne, arrTwo) {
       result.push(arrTwo[indexTwo]);
       indexTwo++;
     }
-
   }
 
   if (indexOne < arrOne.length) {
@@ -24,10 +36,7 @@ function merge(arrOne, arrTwo) {
     result.push(...arrTwo.slice(indexTwo));
   }
 
-
-
   return result;
-
 }
 
 // console.log(merge([1, 3, 4, 5, 6, 7, 8], [1, 2, 3, 5, 7, 9]));
@@ -36,7 +45,6 @@ function merge(arrOne, arrTwo) {
 // console.log(merge([8, 32], [7, 10]));
 
 function mergeSort(arr) {
-
   if (arr.length <= 1) {
     return arr;
   }
@@ -47,9 +55,6 @@ function mergeSort(arr) {
   let rightSide = mergeSort(arr.slice(middle));
 
   return merge(leftSide, rightSide);
-
 }
 
-
 console.log(mergeSort([8, 32, 4, 5, 61, 2]));
-
